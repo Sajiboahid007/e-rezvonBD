1238bebb8bbff0835f74271f789956d888fd71f1
Sajiboahid007 <sajiboahid007@gmail.com>
2026-08-18 22:53:43 +0600

    project initialize and server set up

---
+++ server.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
